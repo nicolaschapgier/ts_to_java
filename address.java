ac83f4539712b81647d4564ba481db1684f5828d
Nicolas Chapgier <nchapgier@hotmail.fr>
2023-02-27 21:08:21 +0100

    first commit

---
+++ address.java
@@ -0,0 +1,8 @@
+public class Adress {
+  private String number;
+  private String street;
+  private String city;
+  private String country;
+
+  public Adress(String number, )
+}
---
+++ user.java
@@ -0,0 +1,9 @@
+import java.util.Adress;
+import java.util.Photo;
+
+public class User {
+  private Date birthdate;
+  private int size;
+  private Photo[] photo;
+  private Address address;
+}
