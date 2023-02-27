6ffe3bf0febfbbc24150ebd38da72357281478a2
Nicolas Chapgier <nchapgier@hotmail.fr>
2023-02-27 22:27:03 +0100

    add User constructor

--- user.java
+++ user.java
@@ -6,4 +6,11 @@ public class User {
   private int size;
   private Photo[] photo;
   private Address address;
+
+  public User (Date birthdate, int size, Photo[] photo, Address address){
+    this.birthdate = birthdate;
+    this.size = size;
+    this.photo = photo;
+    this.address = address;
+  }
 }
